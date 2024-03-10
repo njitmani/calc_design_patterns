d23031ac690b0eedfc20f183d5c5f0a806cfbc44
njitmani <Mk2368@njit.edu>
2024-03-10 13:28:53 -0500

    Added code to main.py to fetch the current environment from env variables

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from dotenv import load_dotenv
+import os
 from app import App
+
+load_dotenv()
+environment=os.getenv('ENVIRONMENT')
+print(f'Running in {environment} mode')
 
 if __name__ == "__main__":
     App.start()
